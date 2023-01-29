1c3dc29b8f7321c39c83a552611d8b14285960e3
JESUS TUZ ACOSTA <jesusreyestuzacosta@gmail.com>
2023-01-28 20:21:27 -0600

    Update conexion.php

--- certi/config/conexion.php
+++ certi/config/conexion.php
@@ -1,33 +1,25 @@
 <?php
-    /*TODO: Inicializando la sesion del usuario */
+
     session_start();
 
-    /*TODO: Iniciamos Clase Conectar */
     class Conectar{
         protected $dbh;
 
-        /*TODO: Funcion Protegida de la cadena de Conexion */
         protected function conexion(){
             try {
-                /*TODO: Cadena de Conexion QA*/
-				$conectar = $this->dbh = new PDO("mysql:host=localhost;dbname=congreso","congreso","abc123$");
-                /*TODO: Cadena de Conexion Produccion*/
-				return $conectar;
+          	$conectar = $this->dbh = new PDO("mysql:host=localhost;dbname=congreso","congreso","abc123$");
+                
+				  return $conectar;
 			} catch (Exception $e) {
-                /*TODO: En Caso hubiera un error en la cadena de conexion */
 				print "¡Error BD!: " . $e->getMessage() . "<br/>";
 				die();
 			}
         }
-
-        /*TODO: Para impedir que tengamos problemas con las ñ o tildes */
+  }
         public function set_names(){
             return $this->dbh->query("SET NAMES 'utf8'");
         }
-
-        /*TODO: Ruta principal del proyecto */
         public static function ruta(){
-            
             return "https://congresosyeventos.valladolid.tecnm.mx/certi/";
             
         }
